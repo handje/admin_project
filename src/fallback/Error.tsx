a1a72533674829ca19525d22f4e2813029865f79
Sonjieun <sonjieun2661@gmail.com>
2024-07-31 19:53:08 +0900

    errCode

--- src/fallback/Error.tsx
+++ src/fallback/Error.tsx
@@ -1,4 +1,8 @@
 import { useRouteError } from "react-router-dom";
+import styled from "styled-components";
+
+import notFound from "../assets/notFound.jpg";
+import forbidden from "../assets/forbidden.jpg";
 
 interface CustomError {
   status: number;
@@ -8,24 +12,50 @@ interface CustomError {
 const Error = () => {
   let title = "Error!";
   let message = "Something went wrong!";
+  let imgSrc;
   const error = useRouteError() as CustomError;
-  console.log(error);
+  console.error(error);
 
   if (error.status === 404) {
-    title = "Page not found";
+    title = "404 Page not found";
+    imgSrc = notFound;
     message = "The page you are looking for does not exist.";
   }
   if (error.status === 500) {
-    title = "Internal server error";
+    title = "500 Internal server error";
     message = error.data.message;
   }
 
+  if (error.status === 403) {
+    //등록권한없음(인증 자격은 있지만 접근권한이 없음)
+    title = "403 Forbidden";
+    imgSrc = forbidden;
+    message =
+      "Due to security reasons, you cannot access this page at this time. Please try again later.";
+  }
   return (
-    <>
-      <h1>{title}</h1>
+    <Container>
+      {imgSrc ? <Image src={imgSrc} alt="ERROR CODE" /> : <h1>{title}</h1>}
       <p>{message}</p>
-    </>
+    </Container>
   );
 };
 
 export default Error;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & > h1 {
+    font-size: 50px;
+    margin-bottom: 30px;
+  }
+`;
+const Image = styled.img`
+  width: 300px;
+  height: 300px;
+`;
